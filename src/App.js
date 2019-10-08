44d7ff0204823c8671a0dd2d230c28bfeb09f633
Gumatan <arnaut.demaret@gmail.com>
2019-10-08 10:23:19 +0200

    user card component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Contact name='Kelly Jimenez' img='https://randomuser.me/api/portraits/men/7.jpg' online='true' />
+      <Contact name='William Little' img='https://randomuser.me/api/portraits/men/79.jpg' online='false' />
+      <Contact name='Shelly Ward' img='https://randomuser.me/api/portraits/women/0.jpg' online='true' />
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = props =>
+    <div className='Contact'>
+        <img className='avatar' src={props.img} alt={props.name}/>
+        <div>
+            <h3 className='name'>{props.name}</h3>
+            <div className='status'>
+                <div className={ props.online === 'true' ? 'status-online' : 'status-offline'}></div>
+                <span className='status-text'>{ props.online === 'true' ? 'Online' : 'Offline'}</span>
+            </div>
+        </div>
+    </div>
+;
+
+export default Contact ; 
